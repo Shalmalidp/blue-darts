7658aa64cd223dbd6e2cbc1ea2d9c5a9deccd74c
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2016-05-03 15:37:18 -0400

    ABOUT social media n button complete

--- src/js/main.js
+++ src/js/main.js
@@ -3,27 +3,45 @@ import $ from 'jquery';
 //GRABBING ELEMENTS FROM HTML TO DISPLAY DATA IN
 var ul = $('.companies');
 var test_ul = $('.t_u');
-var pics = $('.photo');
+var about = $('.product-info');
+var btn = $('button');
 
 //CREATING URL FROM API'S
 var compUrl = 'https://json-data.herokuapp.com/darts/companies';
 var testimonial_url ='https://json-data.herokuapp.com/darts/testimonials';
+var product_url ='https://json-data.herokuapp.com/darts/info';
 var users='';
 
+btn.on('click',function(event){
+  event.preventDefault();
+  alert('100 DARTS ADDED TO YOUR CART ...THANK YOU');
+})
 
 //GETTING JSON FROM TESTIMONIALS API
 $.getJSON(testimonial_url).then(function(res){
-  console.log(res.results);
+  //console.log(res.results);
   res.results.forEach(function(obj){
     var pageData = testimonials_data(obj);
     test_ul.append(pageData);
  });
 });
 
+//PRODUCT INFO FROM PRODUCT API
+
+$.getJSON(product_url).then(function(res){
+  //console.log(res.data.product.description);
+  var pageData=`
+    <h4>${res.data.product.title}</h4>
+    <p>${res.data.product.description}</p>
+
+  `;
+  about.append(pageData);
+});
+
 
 //GETTING JSON FROM USERS API
 
-//inclueded in the testimonial api
+//included in the testimonial api
 
 
 
