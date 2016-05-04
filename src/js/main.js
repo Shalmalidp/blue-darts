a4cd786ceeb8daec63a5d63f4fd4cc4525230fd7
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2016-05-03 21:15:03 -0400

    90% complete CAROUSEL PENDING

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,36 @@
 import $ from 'jquery';
 
-//GRABBING ELEMENTS FROM HTML TO DISPLAY DATA IN
+//GRABBING ELEMENTS FROM HTML TO DISPLAY DATA
 var ul = $('.companies');
 var test_ul = $('.t_u');
 var about = $('.product-info');
 var btn = $('button');
 
+
+
+// CAROUSEL MOVE
+function moveImages(){
+var first   = $('.first');
+var last    = $('.last');
+var current = $('.active');
+var next = current.next();
+
+if(current.hasClass('last')){
+  first.addClass('active');
+  last.removeClass('active');
+}else{
+  current.removeClass('active');
+  next.addClass('active');
+}
+
+};
+setInterval(moveImages(),1000);
+
 //CREATING URL FROM API'S
 var compUrl = 'https://json-data.herokuapp.com/darts/companies';
 var testimonial_url ='https://json-data.herokuapp.com/darts/testimonials';
 var product_url ='https://json-data.herokuapp.com/darts/info';
-var users='';
+var user_url = 'http://api.randomuser.me/?results=';
 
 btn.on('click',function(event){
   event.preventDefault();
@@ -19,11 +39,36 @@ btn.on('click',function(event){
 
 //GETTING JSON FROM TESTIMONIALS API
 $.getJSON(testimonial_url).then(function(res){
-  //console.log(res.results);
-  res.results.forEach(function(obj){
-    var pageData = testimonials_data(obj);
+  var testimonials= res.results;
+
+  $.getJSON(user_url + testimonials.length).then(function(user){
+    console.log(user);
+    var user_data= user.results;
+    var final_test = [];
+
+    for(var i=0; i<testimonials.length; i++){
+      final_test.push({
+        user    : user_data[i].picture.large,
+        name    : testimonials[i].name,
+        review  : testimonials[i].review
+      });
+      console.log('final_test',final_test);
+    } 
+      final_test.forEach(function(x){
+          var pageData = `
+          <li class="test_li">
+          <div class="photo">
+         <img src='${x.user}'/>
+        <div class="testimonial_text">
+          <h4>${x.name}</h4>
+          <p>${x.review}</p>
+        </div>
+      </div>
+    </li>`;
     test_ul.append(pageData);
- });
+
+      });
+  });
 });
 
 //PRODUCT INFO FROM PRODUCT API
@@ -47,27 +92,25 @@ $.getJSON(product_url).then(function(res){
 
 //GETTING JSON FROM COMPANY'S API
 $.getJSON(compUrl).then(function(res){
-  //console.log(res.results);
   res.results.forEach(function(obj){
-    // console.log(obj.image_url);
     var pageData = compImage(obj);
     ul.append(pageData);
   })
 });
 
-function testimonials_data(obj){
-  return `
-    <li class="test_li">
-      <div class="photo">
-        <img src=' '/>
-        <div class="testimonial_text">
-          <h4>${obj.name}</h4>
-          <p>${obj.review}</p>
-        </div>
-      </div>
-    </li>
-  `;
-};
+// function testimonials_data(obj){
+//   return `
+//     <li class="test_li">
+//       <div class="photo">
+//         <img src=' '/>
+//         <div class="testimonial_text">
+//           <h4>${obj.name}</h4>
+//           <p>${obj.review}</p>
+//         </div>
+//       </div>
+//     </li>
+//   `;
+// };
 
 function compImage(obj){
   return `
@@ -76,3 +119,4 @@ function compImage(obj){
   </li>
   `;
 }
+
